093c1d76dcdfda769a3dd97852562ff57c8303b8
Markus Hobl <markus@ispro-ng.at>
2018-02-21 15:34:13 +0100

    Inital commit

---
+++ spec/markdown-mdpdf-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownMdpdfView from '../lib/markdown-mdpdf-view';
+
+describe('MarkdownMdpdfView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
